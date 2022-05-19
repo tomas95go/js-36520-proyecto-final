7e2d399230186c5eb8fef3cfa20d3ac052510bb2
tomas95go <tomas95go@gmail.com>
2022-05-19 17:54:42 -0300

    Reemplazando archivo json por archivo js Este cambio se debe a que no hemos dado json en las clases a esta altura

---
+++ Data/Quiz.js
@@ -0,0 +1,117 @@
+const quiz = {
+  quotes: [
+    {
+      "score-top": 10,
+      "score-bottom": 10,
+      quote: "¡Sos un verdadero fan de Friends (igual era bastante fácil)!",
+    },
+    {
+      "score-top": 9,
+      "score-bottom": 8,
+      quote: "¡Bastante bien, aunque puede ser mejor!",
+    },
+    {
+      "score-top": 7,
+      "score-bottom": 6,
+      quote: "¡Bueno bueno, raspando pero pasaste!",
+    },
+    {
+      "score-top": 5,
+      "score-bottom": 0,
+      quote: "¡Anda a mirar la serie mejor!",
+    },
+  ],
+  questions: [
+    {
+      question: "¿Quién es el actor que interpreta a Joey?",
+      "possible-answers": {
+        A: "Matt LeBlanc",
+        B: "Matt Damon",
+        C: "Matt Reeves",
+      },
+      "correct-answer": "A",
+    },
+    {
+      question: "¿Cuál es el apellido de Chandler?",
+      "possible-answers": {
+        A: "Bong",
+        B: "Beng",
+        C: "Bing",
+      },
+      "correct-answer": "C",
+    },
+    {
+      question: "¿En que ciudad está ambientada la serie?",
+      "possible-answers": {
+        A: "Baltimore",
+        B: "New York",
+        C: "Chicago",
+      },
+      "correct-answer": "B",
+    },
+    {
+      question: "¿Quién es la actriz que interpreta a Rachel?",
+      "possible-answers": {
+        A: "Jennifer Aniston",
+        B: "Jennifer Lopez",
+        C: "Jennifer Lawerence",
+      },
+      "correct-answer": "A",
+    },
+    {
+      question: "¿Quién es el actor que interpreta a Ross?",
+      "possible-answers": {
+        A: "David Schwimmer",
+        B: "David Beckam",
+        C: "David Bowie",
+      },
+      "correct-answer": "A",
+    },
+    {
+      question: "¿Cuántas temporadas tiene FRIENDS?",
+      "possible-answers": {
+        A: 10,
+        B: 8,
+        C: 5,
+      },
+      "correct-answer": "A",
+    },
+    {
+      question: "¿Cuál fue el primer beso de Monica Geller?",
+      "possible-answers": {
+        A: "Ross Geller",
+        B: "Richard Burke",
+        C: "Chandler Bing",
+      },
+      "correct-answer": "A",
+    },
+    {
+      question: "¿Cómo se llama la novela en donde actua Joey?",
+      "possible-answers": {
+        A: "Grey's Anatomy",
+        B: "Days of our lives",
+        C: "House",
+      },
+      "correct-answer": "B",
+    },
+    {
+      question: "¿Cuántos hijos tuvo Phoebe Buffay?",
+      "possible-answers": {
+        A: 3,
+        B: 2,
+        C: 1,
+      },
+      "correct-answer": "A",
+    },
+    {
+      question:
+        "¿Cómo se llama el café donde se suelen juntar los protagonistas?",
+      "possible-answers": {
+        A: "Central Park",
+        B: "Central Garden",
+        C: "Central Perk",
+      },
+      "correct-answer": "C",
+    },
+  ],
+};
