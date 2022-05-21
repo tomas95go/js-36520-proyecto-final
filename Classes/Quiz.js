6ff46e8840b4fd868744a8a90d5a7135de662d4a
tomas95go <tomas95go@gmail.com>
2022-05-21 19:25:27 -0300

    Primera entrega del Proyecto Final finalizada Se considera finalizada la primera entrega del proyecto final cumpliendo con las rúbricas de evaluación.

--- Classes/Quiz.js
+++ Classes/Quiz.js
@@ -1,8 +1,11 @@
 export class Quiz {
-  constructor(questions, quotes, score) {
+  constructor(questions, quotes, score, userAnswers, gameOver, rematch) {
     this.questions = questions;
     this.quotes = quotes;
     this.score = score;
+    this.userAnswers = userAnswers;
+    this.gameOver = gameOver;
+    this.rematch = rematch;
   }
 
   addQuestion(question) {
@@ -13,13 +16,109 @@ export class Quiz {
     return this.quotes.push(quote);
   }
 
-  incrementScore() {
-    return this.score++;
+  addQuestions(data, Question) {
+    data.forEach((DataQuestion) => {
+      const question = new Question(
+        DataQuestion.question,
+        DataQuestion["possible-answers"],
+        DataQuestion["correct-answer"]
+      );
+      this.addQuestion(question);
+    });
+    return this.questions;
   }
 
-  showQuote(quotes) {
-    return quotes.find(
+  addQuotes(data, Quote) {
+    data.forEach((DataQuote) => {
+      const quote = new Quote(
+        DataQuote["score-top"],
+        DataQuote["score-bottom"],
+        DataQuote.quote
+      );
+      this.addQuote(quote);
+    });
+    return this.quotes;
+  }
+
+  incrementScore(userResponse, i) {
+    if (userResponse === this.questions[i].correctAnswer) {
+      this.score++;
+    } else {
+      this.score += 0;
+    }
+    return this.score;
+  }
+
+  showQuote() {
+    return this.quotes.find(
       (quote) => this.score >= quote.scoreBottom && this.score <= quote.scoreTop
     ).quote;
+  }
+
+  collectUserAnswers() {
+    this.questions.map((question) => {
+      const userAnswer = prompt(
+        `${question.question}\n${Object.entries(
+          question.possibleAnswers
+        ).reduce((prev, curr) => {
+          return `${prev + curr[0]}) ${curr[1]}\n`;
+        }, ``)}`
+      )
+        .toString()
+        .toUpperCase();
+      this.userAnswers.push(userAnswer);
+    });
+    return this.userAnswers;
+  }
+
+  evaluate() {
+    this.userAnswers.forEach((answer, i) => {
+      return this.incrementScore(answer, i);
+    });
+  }
+
+  endGame() {
+    this.gameOver = !this.gameOver;
+    return this.gameOver;
+  }
+
+  evaluateRematch() {
+    if (this.rematch.toUpperCase() !== "A") {
+      return this.endGame();
+    } else {
+      this.resetUserAnswers();
+      this.resetScore();
+      return this.rematch;
+    }
+  }
+
+  resetScore() {
+    this.score = 0;
+    return this.score;
+  }
+
+  resetUserAnswers() {
+    this.userAnswers.length = 0;
+    return this.userAnswers;
+  }
+
+  askForRematch() {
+    this.rematch = prompt(`¿Desea volver a jugar?\nA) SI \nB) NO`);
+    return this.rematch;
+  }
+
+  showInstructions() {
+    alert(
+      `Bienvenido/a. Vamos a poner a prueba tu conocimiento en la serie FRIENDS. Elige A, B o C como respuesta las preguntas.`
+    );
+  }
+
+  play() {
+    this.showInstructions();
+    this.collectUserAnswers();
+    this.evaluate();
+    alert(`Puntaje final: ${this.score}.${this.showQuote()}`);
+    this.askForRematch();
+    this.evaluateRematch();
   }
 }
--- index.js
+++ index.js
@@ -5,51 +5,11 @@ import { QuizData } from "./Data/Quiz.js";
 import { Question } from "./Classes/Question.js";
 import { Quote } from "./Classes/Quote.js";
 
-const quiz = new Quiz([], [], 0);
-let gameOver = false;
+const quiz = new Quiz([], [], 0, [], false, "A");
 
-QuizData.questions.forEach((data) => {
-  const question = new Question(
-    data.question,
-    data["possible-answers"],
-    data["correct-answer"]
-  );
-  quiz.addQuestion(question);
-});
+quiz.addQuestions(QuizData.questions, Question);
+quiz.addQuotes(QuizData.quotes, Quote);
 
-QuizData.quotes.forEach((data) => {
-  const quote = new Quote(data["score-top"], data["score-bottom"], data.quote);
-  quiz.addQuote(quote);
-});
-
-const questionListLength = quiz.questions.length;
-
-while (!gameOver) {
-  quiz.questions.forEach((question) => {
-    const response = prompt(
-      `${question.question}\n${Object.entries(question.possibleAnswers).reduce(
-        (prev, curr) => {
-          return `${prev + curr[0]}) ${curr[1]}\n`;
-        },
-        ``
-      )}`
-    )
-      .toString()
-      .toUpperCase();
-
-    if (response === question.correctAnswer) {
-      quiz.incrementScore();
-    }
-  });
-
-  if (quiz.questions.length === questionListLength) {
-    gameOver = !gameOver;
-    alert(`Tu puntaje es: ${quiz.score}.\n${quiz.showQuote(quiz.quotes)}`);
-    quiz.score = 0;
-  }
-
-  const newGame = prompt("¿Desea volver a jugar\nA) SI B) NO");
-  if (newGame.toUpperCase() === "A") {
-    gameOver = !gameOver;
-  }
+while (!quiz.gameOver) {
+  quiz.play();
 }
