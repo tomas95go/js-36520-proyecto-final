4127a100826335096fcf186a2ee0b9f15741d7fa
tomas95go <tomas95go@gmail.com>
2022-06-01 16:52:15 -0300

    Extrayendo el código de los Toasts para aumentar la legibilidad

--- game.js
+++ game.js
@@ -3,6 +3,12 @@ import { Instruction } from "./classes/Instruction.js";
 import { Question } from "./classes/Question.js";
 import { Quote } from "./classes/Quote.js";
 import { Quiz } from "./classes/Quiz.js";
+import {
+  showNoOptionSelectedToast,
+  showEncouragementToast,
+  showNotValidNameToast,
+  showQuestionAnsweredToast,
+} from "./toasts.js";
 
 const getQuizData = async () => {
   const response = await fetch(`./data/quiz.json`);
@@ -247,34 +253,10 @@ const showPlayerFormCard = (quiz, player) => {
       if (isValid) {
         player.name = name;
         localStorage.setItem("player", JSON.stringify(player));
-        Toastify({
-          text: `¡Éxitos ${player.name}!`,
-          duration: 3000,
-          newWindow: true,
-          close: true,
-          gravity: "top", // `top` or `bottom`
-          position: "right", // `left`, `center` or `right`
-          stopOnFocus: true, // Prevents dismissing of toast on hover
-          className: "has-background-success",
-          style: {
-            background: "unset",
-          },
-        }).showToast();
+        showEncouragementToast(player.name);
         loadQuestion(quiz, player);
       } else {
-        Toastify({
-          text: "El campo nombre no puede estar vacío",
-          duration: 3000,
-          newWindow: true,
-          close: true,
-          gravity: "top", // `top` or `bottom`
-          position: "right", // `left`, `center` or `right`
-          stopOnFocus: true, // Prevents dismissing of toast on hover
-          className: "has-background-warning-dark",
-          style: {
-            background: "unset",
-          },
-        }).showToast();
+        showNotValidNameToast();
       }
     });
     makeCardFooter(`¡Empezar!`, `welcome`, `player-form`);
@@ -337,35 +319,11 @@ const showQuestionCard = (quiz, player, questionId) => {
       const answer = formPotencia.get("question");
       const isValid = validate(answer);
       if (isValid) {
-        Toastify({
-          text: "Se ha guardado tu respuesta :)",
-          duration: 3000,
-          newWindow: true,
-          close: true,
-          gravity: "top", // `top` or `bottom`
-          position: "right", // `left`, `center` or `right`
-          stopOnFocus: true, // Prevents dismissing of toast on hover
-          className: "has-background-success",
-          style: {
-            background: "unset",
-          },
-        }).showToast();
+        showQuestionAnsweredToast();
         player.add(answer);
         loadNextQuestion(quiz, player, questionId);
       } else {
-        Toastify({
-          text: "Por favor, elija una opción para continuar",
-          duration: 3000,
-          newWindow: true,
-          close: true,
-          gravity: "top", // `top` or `bottom`
-          position: "right", // `left`, `center` or `right`
-          stopOnFocus: true, // Prevents dismissing of toast on hover
-          className: "has-background-warning-dark",
-          style: {
-            background: "unset",
-          },
-        }).showToast();
+        showNoOptionSelectedToast();
       }
     });
   }
---
+++ toasts.js
@@ -0,0 +1,63 @@
+export const showNoOptionSelectedToast = () => {
+  return Toastify({
+    text: "Por favor, elija una opción para continuar",
+    duration: 3000,
+    newWindow: true,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    className: "has-background-warning-dark",
+    style: {
+      background: "unset",
+    },
+  }).showToast();
+};
+
+export const showEncouragementToast = (name) => {
+  return Toastify({
+    text: `¡Éxitos ${name}!`,
+    duration: 3000,
+    newWindow: true,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    className: "has-background-success",
+    style: {
+      background: "unset",
+    },
+  }).showToast();
+};
+
+export const showNotValidNameToast = () => {
+  return Toastify({
+    text: "El campo nombre no puede estar vacío",
+    duration: 3000,
+    newWindow: true,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    className: "has-background-warning-dark",
+    style: {
+      background: "unset",
+    },
+  }).showToast();
+};
+
+export const showQuestionAnsweredToast = () => {
+  return Toastify({
+    text: "Se ha guardado tu respuesta :)",
+    duration: 3000,
+    newWindow: true,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    className: "has-background-success",
+    style: {
+      background: "unset",
+    },
+  }).showToast();
+};
